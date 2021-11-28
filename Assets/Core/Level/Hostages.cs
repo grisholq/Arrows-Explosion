41d00d5cacf8b4f64155a44160f18fc78227714a
grisholq <drowen2014@mail.ru>
2021-11-28 13:18:50 +0300

    Стикман доработка

--- Assets/Core/Level/Hostages.cs
+++ Assets/Core/Level/Hostages.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Hostages : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _hostagesDied;
+    [SerializeField] private int _loseAmount;
 
+    private int _currentAmount;
+
+    private void Update()
+    {
+        
+    }
 }
--- Assets/Core/Level/Level.cs
+++ Assets/Core/Level/Level.cs
@@ -4,5 +4,21 @@ using UnityEngine.Events;
 public class Level : MonoBehaviour
 {
     [SerializeField] private UnityEvent _victory;
-    [SerializeField] private UnityEvent _lose;
+    [SerializeField] private UnityEvent _friendsLose;
+    [SerializeField] private UnityEvent _ammoLose;
+
+    public void Win()
+    {
+        _victory.Invoke();
+    }
+
+    public void LoseFriends()
+    {
+        _friendsLose.Invoke();
+    }
+
+    public void LoseAmmo()
+    {
+        _ammoLose.Invoke();
+    }
 }
