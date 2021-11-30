e15c600ba9fe5d62036e3c09bf1f3ba04e0ed06f
grisholq <drowen2014@mail.ru>
2021-12-01 00:04:09 +0300

    Добавил скорость вращения камеры

--- Assets/Core/Level/Camera/CameraObserver.cs
+++ Assets/Core/Level/Camera/CameraObserver.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 public class CameraObserver : Singleton<CameraObserver>
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _angularSpeed;
 
     public CameraObservable Current { get; set; }
   
@@ -15,17 +16,40 @@ public class CameraObserver : Singleton<CameraObserver>
     public void Observe()
     {
         if (Current == null) return;
+        ChangePosition();
+        ChangeRotation();       
+    }
 
-        transform.position = Vector3.MoveTowards(transform.position, Current.CameraPosition, _speed * Current.SpeedMultiplier * Time.unscaledDeltaTime);
+    private void ChangeRotation()
+    {
+        float angularSpeed = Time.unscaledDeltaTime * _angularSpeed * Current.SpeedMultiplier;
 
         if (Current.LookAtObject)
         {
             transform.LookAt(Current.Transform);
             transform.eulerAngles += Current.LookAtEulers;
         }
-        else 
+        else
         {
-            transform.eulerAngles = Current.Eulers;
-        }       
+            transform.eulerAngles = RotateTowards(Current.Eulers);         
+        }
+    }
+
+    private Vector3 RotateTowards(Vector3 target)
+    {
+        Vector3 eulers = transform.eulerAngles;
+        float speed = Time.unscaledDeltaTime * _angularSpeed * Current.SpeedMultiplier;
+
+        eulers.x = Mathf.MoveTowardsAngle(eulers.x, target.x, speed);
+        eulers.y = Mathf.MoveTowardsAngle(eulers.y, target.y, speed);
+        eulers.z = Mathf.MoveTowardsAngle(eulers.z, target.z, speed);
+
+        return eulers;
+    }
+
+    private void ChangePosition()
+    {
+        float speed = _speed * Current.SpeedMultiplier * Time.unscaledDeltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Current.CameraPosition, speed);
     }
 }
